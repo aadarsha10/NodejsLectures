07b9bb88eaaf5d64a5c4447c59a5b635b066b163
Aadarsha Man Shrestha <a.shrestha998@gmail.com>
2020-12-16 12:54:17 +0545

    finished partials

--- templates/app.js
+++ templates/app.js
@@ -8,8 +8,13 @@ const publicDirectory=path.join(__dirname,'public');
 
 app.use(express.static(publicDirectory)); //to define directory to enable searching for static files(css/images etc) by the app
 
-//hbs inititalize
+//hbs inititalize and use in this application as a view engine
 app.set('view engine', 'hbs');
+
+//to let the system know where the partials are located
+const partial_dir = path.join(__dirname+'/views/partials');
+hbs.registerPartials(partial_dir);
+
 
 app.get('/',function(req,res){
 
